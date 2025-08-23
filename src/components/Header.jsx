2df32cb476a7884a6fa85693fc478fb36974b441
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-08-23 16:27:18 +0300

    upd Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <>
-      <h1 className="flex mb-5 text-4xl text-emerald-800">Header</h1>
+    <header className="bg-blue-200">
+      <h1 className="flex mb-5 text-4xl text-red-800">Header</h1>
       <NavLink to={"/"}>Home</NavLink>
       <NavLink to={"/about"}>About</NavLink>
       <NavLink to={"/cart"}>Cart</NavLink>
       <hr />
-    </>
+    </header>
   );
 }
 
