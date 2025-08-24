22a239825d930ceaf38a2765a61eb8aebe2b747c
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-08-24 18:25:46 +0300

    upd index.html

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,15 @@ function Header() {
   return (
     <header className="bg-blue-200">
       <h1 className="flex mb-5 text-4xl text-red-800">Header</h1>
-      <NavLink to={"/"}>Home</NavLink>
-      <NavLink to={"/about"}>About</NavLink>
-      <NavLink to={"/cart"}>Cart</NavLink>
+      <NavLink className="font-semibold" to={"/"}>
+        Home
+      </NavLink>
+      <NavLink className="font-semibold" to={"/about"}>
+        About
+      </NavLink>
+      <NavLink className="font-semibold" to={"/cart"}>
+        Cart
+      </NavLink>
       <hr />
     </header>
   );
