4585cd6da769b2f63352c9c04de07af2ae7acfdd
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-08-26 17:09:31 +0300

    added gradient to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,14 +8,14 @@ function Home() {
 
   return (
     <div className="py-10">
-      <h1 className="mb-8 text-2xl font-semibold text-center">Categories</h1>
+      <h1 className="mb-8 text-2xl font-semibold text-center text-primary" >Categories</h1>
       <ul className="grid grid-cols-3 gap-4 px-5">
         {categories.map((category) => (
           <li key={category.id}>
             <Link className="relative flex flex-col items-center justify-center" to={`/category/${category.name}`}>
               <span className="absolute z-10 text-xl font-semibold text-white">{category.name}</span>
               <img className="rounded-md" src={category.img} alt={category.name} />
-              <div className="absolute inset-0 rounded-md opacity-40 bg-gray-950"></div>
+              <div className="absolute inset-0 rounded-md opacity-40 bg-gradient-to-t from-gray-900 via-gray-700 via-30% to-gray-300"></div>
             </Link>
           </li>
         ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "custom-gradient": "linear-gradient(to right, #ff7e5f, #feb47b)",
+      },
+      colors: {
+        primary: "#3B92F6",
+        secondary: "#feb47b",
+      },
+    },
   },
   plugins: [],
 };
