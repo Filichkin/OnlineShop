54ee8d4f83458e26b7004fb6487c261f8775b2ea
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-08-27 15:32:26 +0300

    added isActive to NavLink

--- src/UI/NavLinkMenu.jsx
+++ src/UI/NavLinkMenu.jsx
@@ -2,7 +2,9 @@ import { NavLink } from "react-router-dom";
 
 export default function NavMenuLink({ to, children }) {
     return (
-        <NavLink className="font-semibold text-gray-600" to={to}>
+        <NavLink className={({ isActive }) => 
+          (isActive ? "text-gray-900 font-semibold" : "text-gray-600")
+        } to={to}>
           {children}
         </NavLink>
     );
