f301acbbbda5753052c08ba7117c1f3c781cd0a1
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-10-31 13:16:47 +0200

    feat: update header navigation icons

--- frontend/src/UI/NavLinkMenu.jsx
+++ frontend/src/UI/NavLinkMenu.jsx
@@ -1,11 +1,16 @@
 import { NavLink } from "react-router-dom";
 
 export default function NavMenuLink({ to, children }) {
-    return (
-        <NavLink className={({ isActive }) => 
-          (isActive ? "text-gray-900 font-semibold" : "text-gray-600")
-        } to={to}>
-          {children}
-        </NavLink>
-    );
+  return (
+    <NavLink
+      className={({ isActive }) =>
+        `flex flex-col items-center gap-1 text-sm transition-colors ${
+          isActive ? "text-gray-900 font-semibold" : "text-gray-600"
+        }`
+      }
+      to={to}
+    >
+      {children}
+    </NavLink>
+  );
 }
